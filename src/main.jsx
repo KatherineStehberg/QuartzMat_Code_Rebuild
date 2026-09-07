18323548f1a3d05c79f6f104ba8ead70227fd5d1
Katherine Stehberg <160335098+KatherineStehberg@users.noreply.github.com>
2026-09-06 22:20:06 -0300

    feat: wire Flow payment result and checkout styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import PaymentResult from './components/PaymentResult';
 import './styles.css';
+import './checkout.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PaymentResult />
     <App />
   </React.StrictMode>
 );
